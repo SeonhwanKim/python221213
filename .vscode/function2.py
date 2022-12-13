8d644bd632feb38208c075366f33d867b3f8cfb2
SeonhwanKim <ac7979@naver.com>
2022-12-13 16:55:19 +0900

    펑션2

--- .vscode/function2.py
+++ .vscode/function2.py
@@ -1,9 +1,27 @@
 # function2.py
 
-def setValue(newValue):
-    x = newValue
-    print(x)
+# def setValue(newValue):
+#     x = newValue
+#     print(x)
 
-retValue = setValue(5)
-print(retValue)
+# retValue = setValue(5)
+# print(retValue)
 
+
+
+g = 1
+print("--------------- 1")
+print(id(g))
+print(g)
+
+def testScope(a):
+    global g
+    g = 2
+    print("--------------- 2")
+    print(id(g))
+    print(g)
+
+testScope(5)
+print("--------------- 3")
+print(id(g))
+print(g)
